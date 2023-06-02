ae2673d0ca0b99af909f1be5daac87820654dd68
akhileshmagdum <akhileshmagdum@gmail.com>
2023-06-02 22:42:38 +0530

    Creating view and passing data to the view

--- basic-crud/app/controllers/BasicController.java
+++ basic-crud/app/controllers/BasicController.java
@@ -2,10 +2,12 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
+// A view needs to be imported manually
+import views.html.home.homepage;
 
 public class BasicController extends Controller {
 
     public Result homepage(String fName, String lName) {
-        return ok("Welcome Home! \n User -> " + fName + " " + lName);
+        return ok(homepage.render(fName, lName));
     }
 }
