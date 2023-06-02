e712e71e6839ac713d5e5728eadb5b66984cf646
akhileshmagdum <akhileshmagdum@gmail.com>
2023-06-02 22:29:53 +0530

    API with path variables

--- basic-crud/app/controllers/BasicController.java
+++ basic-crud/app/controllers/BasicController.java
@@ -5,7 +5,7 @@ import play.mvc.Result;
 
 public class BasicController extends Controller {
 
-    public Result homepage() {
-        return ok("Welcome Home!");
+    public Result homepage(String fName, String lName) {
+        return ok("Welcome Home! \n User -> " + fName + " " + lName);
     }
 }
