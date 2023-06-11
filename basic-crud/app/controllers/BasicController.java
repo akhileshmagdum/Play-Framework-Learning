369a870fd3dcf672040a2db5b11ac646f63b1603
akhileshmagdum <akhileshmagdum@gmail.com>
2023-06-11 12:25:18 +0530

    Get all API

--- basic-crud/app/controllers/BasicController.java
+++ basic-crud/app/controllers/BasicController.java
@@ -1,13 +1,22 @@
 package controllers;
 
+import models.Employee;
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
-// A view needs to be imported manually
 import views.html.home.homepage;
+
+import java.util.List;
 
 public class BasicController extends Controller {
 
+
     public Result homepage(String fName, String lName) {
         return ok(homepage.render(fName, lName));
+    }
+
+    public Result getAllEmployee() {
+        List<Employee> employeeList = Employee.employeeFinder.all();
+        return ok(Json.toJson(employeeList));
     }
 }
